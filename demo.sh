121f58e5c685015dc9cea75b7bdfa21072f44de1
haman123 <wybj128@gmail.com>
2018-06-11 16:40:25 +0900

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -5,4 +5,4 @@ RSSFILE="daming.xml"
 # export PBUUID=""
 ./qingting_linux_amd64 $ALBUMID > $RSSFILE
 ls
-curl -X PUT -F c=@$RSSFILE https://ptpb.pw/$PBUUID
+curl -X PUT -F c=@$RSSFILE http://fars.ee/$PBUUID
