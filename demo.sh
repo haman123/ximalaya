eb4772cb384853baa2a1f9f55954209b632873ba
haman123 <wybj128@gmail.com>
2018-06-11 16:08:19 +0900

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-ALBUMID="12825974"
-RSSFILE="2018sfcy.xml"
+ALBUMID="63048"
+RSSFILE="starming.xml"
 # export PBUUID=""
-./ximalaya_linux_amd64 $ALBUMID > $RSSFILE
+./qingting_linux_amd64 $ALBUMID > $RSSFILE
 ls
 curl -X PUT -F c=@$RSSFILE https://ptpb.pw/$PBUUID
